9efe7a1a4dc2786a4637af879d9fc1269eeb57ab
tcdw <tcdw2011@gmail.com>
2018-03-08 23:11:31 +0800

    新脚手架

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-const express = require('express');
-const app = express();
+const logger = require('koa-logger');
+const router = require('koa-router')();
+const koaBody = require('koa-body');
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
+const Koa = require('koa');
+const app = module.exports = new Koa();
 
-app.listen(3000);
+app.use(logger());
+app.use(koaBody());
+
+router.get('/', list)
+    .get('/post/:id', show)
+
+app.use(router.routes());
+
+async function list(ctx) {
+    console.log(ctx);
+    ctx.response.body = 'Hello World';
+}
+
+async function show(ctx) {
+    ctx.response.body = 'Hello World! id: ' + ctx.params.id;
+}
+
+if (!module.parent) app.listen(3000);
