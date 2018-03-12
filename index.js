59c83fb335ac54996d43ab26b71989a438ff541a
tcdw <tcdw2011@gmail.com>
2018-03-12 23:00:07 +0800

    增加命令行帮助

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const argv = require('minimist')(process.argv.slice(2));
 const printLog = require('./lib/log');
 const setup = require('./lib/setup');
 const server = require('./server');
+const fs = require('fs');
+const path = require('path');
+
+const showHelp = () => {
+    console.log(`Usage: pomment COMMAND [OPTION]...
+
+Avaliable commands:
+
+ - init [PATH]\tInit a Pomment data directory
+ - web\t\tRun the web service`);
+};
 
 switch (argv._[0]) {
 case 'init': {
@@ -15,7 +26,18 @@ case 'web': {
     printLog('info', 'The HTTP server is listening port 3000');
     break;
 }
+case 'version': {
+    const meta = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json')));
+    console.log(meta.version);
+    break;
+}
+case 'help': {
+    showHelp();
+    break;
+}
 default: {
+    showHelp();
+    process.exit(1);
     break;
 }
 }
