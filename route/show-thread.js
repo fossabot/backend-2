fa7d583118c377bae4926902a7a27f0a825c8550
tcdw <tcdw2011@gmail.com>
2018-03-13 23:47:02 +0800

    调整

--- route/show-thread.js
+++ route/show-thread.js
@@ -1,6 +1,7 @@
 const beautify = require('json-beautify');
 
 module.exports = async (ctx) => {
+    ctx.type = 'application/json';
     ctx.response.body = beautify({
         name: ctx.params.name,
         conf: ctx.userConfig,
