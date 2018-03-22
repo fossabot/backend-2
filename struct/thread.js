708be4f8cec8740f0f6bfc8b9689b1c52f89bcd3
tcdw <tcdw2011@gmail.com>
2018-03-22 17:03:07 +0800

    在索引表增加标题和地址记录

--- struct/thread.js
+++ struct/thread.js
@@ -2,5 +2,7 @@ const Sequelize = require('sequelize');
 
 module.exports = {
     name: { type: Sequelize.STRING(), allowNull: false, unique: true },
+    title: { type: Sequelize.STRING(), allowNull: false },
+    url: { type: Sequelize.STRING(), allowNull: false },
     post: { type: Sequelize.INTEGER(), allowNull: false },
 };
