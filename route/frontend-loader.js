11b73ddaf7a8043805f385dafeefc3c508a6c39b
tcdw <tcdw2011@gmail.com>
2018-05-06 22:22:24 +0800

    改进逻辑

--- route/frontend-loader.js
+++ route/frontend-loader.js
@@ -6,16 +6,18 @@ module.exports = async (ctx) => {
     const threadName = ctx.params.name;
     ctx.type = 'text/javascript';
     ctx.response.body = `(function () {
-    var keyList = ['title', 'url', 'avatarPrefix'];
+    var keyList = ['title', 'avatarPrefix'];
     var e = document.querySelector('${addEscape(element)}');
     var s = document.createElement('script');
+    var c = document.querySelector('link[rel=canonical]');
     s.src = '${addEscape(script)}';
     s.onload = function () {
-        var pomment = new Pomment(
-            element,
-            '${addEscape(server)}',
-            '${addEscape(threadName)}',
-        );
+        var pomment = new Pomment(e, '${addEscape(server)}', '${addEscape(threadName)}');
+        if (e.dataset.url) {
+            pomment.url = e.dataset.url;
+        } else if (c) {
+            pomment.url = c.href;
+        }
         for (var i = 0; i < keyList.length; i++) {
             if (e.dataset[keyList[i]]) {
                 pomment[keyList[i]] = e.dataset[keyList[i]];
