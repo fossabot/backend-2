c9a1d891e7e99bd61be42e5e0fbfdec728b4df73
tcdw <tcdw2011@gmail.com>
2018-05-06 22:01:45 +0800

    允许用户使用 dataset 加载信息

--- route/frontend-loader.js
+++ route/frontend-loader.js
@@ -2,18 +2,26 @@ const addEscape = target => target.split("'").join("\\'");
 
 module.exports = async (ctx) => {
     const config = ctx.userConfig.info;
-    const { element, server, script } = config.frontendLoader;
+    const { script, element, server } = config.frontendLoader;
     const threadName = ctx.params.name;
     ctx.type = 'text/javascript';
     ctx.response.body = `(function () {
+    var keyList = ['title', 'url', 'avatarPrefix'];
+    var e = document.querySelector('${addEscape(element)}');
     var s = document.createElement('script');
     s.src = '${addEscape(script)}';
     s.onload = function () {
-        new Pomment(
-            '${addEscape(element)}',
+        var pomment = new Pomment(
+            element,
             '${addEscape(server)}',
-            '${addEscape(threadName)}'
-        ).init();
+            '${addEscape(threadName)}',
+        );
+        for (var i = 0; i < keyList.length; i++) {
+            if (e.dataset[keyList[i]]) {
+                pomment[keyList[i]] = e.dataset[keyList[i]];
+            }
+        }
+        pomment.init();
     }
     document.body.appendChild(s);
 })();`;
