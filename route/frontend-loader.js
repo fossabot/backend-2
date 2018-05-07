82af82c5df28ad0d172a70a553498aa48880effb
tcdw <tcdw2011@gmail.com>
2018-05-07 18:08:32 +0800

    移除了读取 dataset 的选项

--- route/frontend-loader.js
+++ route/frontend-loader.js
@@ -6,25 +6,18 @@ module.exports = async (ctx) => {
     const threadName = ctx.params.name;
     ctx.type = 'text/javascript';
     ctx.response.body = `(function () {
-    var keyList = ['title', 'avatarPrefix'];
-    var e = document.querySelector('${addEscape(element)}');
     var s = document.createElement('script');
+    var e = document.querySelector('${addEscape(element)}');
     var c = document.querySelector('link[rel=canonical]');
     s.src = '${addEscape(script)}';
     s.onload = function () {
         var pomment = new Pomment(e, '${addEscape(server)}', '${addEscape(threadName)}');
-        if (e.dataset.url) {
-            pomment.url = e.dataset.url;
-        } else if (c) {
+        if (c) {
             pomment.url = c.href;
-        }
-        for (var i = 0; i < keyList.length; i++) {
-            if (e.dataset[keyList[i]]) {
-                pomment[keyList[i]] = e.dataset[keyList[i]];
-            }
         }
         pomment.init();
     }
     document.body.appendChild(s);
-})();`;
+})();
+`;
 };
