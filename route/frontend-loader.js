4ec1accec0b55bb05536c9c0a3232f675456b2b2
tcdw <tcdw2011@gmail.com>
2018-05-13 00:55:46 +0800

    fixedheight

--- route/frontend-loader.js
+++ route/frontend-loader.js
@@ -3,7 +3,9 @@ const config = require('../lib/config');
 const addEscape = target => target.split("'").join("\\'");
 
 module.exports = async (ctx) => {
-    const { script, element, server } = config.common.frontendLoader;
+    const {
+        script, element, server, fixedHeight,
+    } = config.common.frontendLoader;
     const threadName = ctx.params.name;
     ctx.type = 'text/javascript';
     ctx.response.body = `(function () {
@@ -12,7 +14,9 @@ module.exports = async (ctx) => {
     var c = document.querySelector('link[rel=canonical]');
     s.src = '${addEscape(script)}';
     s.onload = function () {
-        var pomment = new Pomment(e, '${addEscape(server)}', '${addEscape(threadName)}');
+        var pomment = new Pomment(e, '${addEscape(server)}', '${addEscape(threadName)}', {
+            fixedHeight: ${fixedHeight}
+        });
         if (c) {
             pomment.url = c.href;
         }
