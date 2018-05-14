b46be6554071c7ac58c14e806259270e40321337
tcdw <tcdw2011@gmail.com>
2018-05-14 19:03:47 +0800

    改进前端加载器

--- route/frontend-loader.js
+++ route/frontend-loader.js
@@ -1,22 +1,16 @@
 const config = require('../lib/config');
 
-const addEscape = target => target.split("'").join("\\'");
-
 module.exports = async (ctx) => {
-    const {
-        script, element, server, fixedHeight,
-    } = config.common.frontendLoader;
-    const info = ctx.request.body;
-    const threadName = info.url;
     ctx.type = 'text/javascript';
     ctx.response.body = `(function () {
+    var opt = ${JSON.stringify(config.common.frontendLoader)};
     var s = document.createElement('script');
-    var e = document.querySelector('${addEscape(element)}');
+    var e = document.querySelector(opt.element);
     var c = document.querySelector('link[rel=canonical]');
-    s.src = '${addEscape(script)}';
+    s.src = opt.script;
     s.onload = function () {
-        var pomment = new Pomment(e, '${addEscape(server)}', '${addEscape(threadName)}', {
-            fixedHeight: ${fixedHeight}
+        var pomment = new Pomment(e, opt.server, {
+            fixedHeight: opt.fixedHeight
         });
         if (c) {
             pomment.url = c.href;
