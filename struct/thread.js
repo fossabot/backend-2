6fc8cdba7842cddeff1c56b3185242eedddc2184
tcdw <tcdw2011@gmail.com>
2018-05-14 18:33:42 +0800

    使用 url 作为唯一标识

--- struct/thread.js
+++ struct/thread.js
@@ -1,8 +1,7 @@
 const Sequelize = require('sequelize');
 
 module.exports = {
-    name: { type: Sequelize.STRING(), allowNull: false, unique: true },
+    url: { type: Sequelize.STRING(), allowNull: false, unique: true },
     title: { type: Sequelize.STRING(), allowNull: false },
-    url: { type: Sequelize.STRING(), allowNull: false },
     post: { type: Sequelize.INTEGER(), allowNull: false },
 };
