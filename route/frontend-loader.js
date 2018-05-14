85b1268d641afd5ec89b3a1b3e70767b571065f4
tcdw <tcdw2011@gmail.com>
2018-05-14 19:05:13 +0800

    移除不必要的处理

--- route/frontend-loader.js
+++ route/frontend-loader.js
@@ -6,15 +6,11 @@ module.exports = async (ctx) => {
     var opt = ${JSON.stringify(config.common.frontendLoader)};
     var s = document.createElement('script');
     var e = document.querySelector(opt.element);
-    var c = document.querySelector('link[rel=canonical]');
     s.src = opt.script;
     s.onload = function () {
         var pomment = new Pomment(e, opt.server, {
             fixedHeight: opt.fixedHeight
         });
-        if (c) {
-            pomment.url = c.href;
-        }
         pomment.init();
     }
     document.body.appendChild(s);
